91df8187e17d6a4edea982537118022d3e375e66
bcolby45 <bcolby7@gmail.com>
2021-07-14 22:57:46 -0400

    Add lWzProperty class

---
+++ src/IWzProperty.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "pch.h"
+#include <OAIdl.h>
+
+//credits to shavit
+class IWzProperty
+{
+public:
+	// IUnknown
+	virtual HRESULT QueryInterface(const IID* riid, void** ppvObject) = 0;
+	virtual ULONG AddRef() = 0;
+	virtual ULONG Release() = 0;
+
+	// IWzSerialize
+	virtual HRESULT get_persistentUOL(wchar_t**) = 0;
+	virtual HRESULT raw_Serialize(IWzArchive*) = 0;
+
+	// IWzProperty
+	virtual HRESULT get_item(wchar_t*, tagVARIANT*) = 0;
+	virtual HRESULT put_item(wchar_t*, tagVARIANT) = 0;
+	virtual HRESULT get__NewEnum(IUnknown**) = 0;
+	virtual HRESULT get_count(unsigned int*) = 0;
+	virtual HRESULT raw_Add(wchar_t*, tagVARIANT, tagVARIANT) = 0;
+	virtual HRESULT raw_Remove(wchar_t*) = 0;
+	virtual HRESULT raw_Import(wchar_t*) = 0;
+};
